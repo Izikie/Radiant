54f9a264f6a27a2099790f72eb32ad2a400c90ae
Izikie <78509731+Izikie@users.noreply.github.com>
2025-01-20 17:53:46 -0600

    Try with resource

--- src/main/java/net/minecraft/nbt/CompressedStreamTools.java
+++ src/main/java/net/minecraft/nbt/CompressedStreamTools.java
@@ -34,12 +34,9 @@ public class CompressedStreamTools {
     }
 
     public static void writeCompressed(NBTTagCompound p_74799_0_, OutputStream outputStream) throws IOException {
-        DataOutputStream dataoutputstream = new DataOutputStream(new BufferedOutputStream(new GZIPOutputStream(outputStream)));
 
-        try {
+        try (DataOutputStream dataoutputstream = new DataOutputStream(new BufferedOutputStream(new GZIPOutputStream(outputStream)))) {
             write(p_74799_0_, dataoutputstream);
-        } finally {
-            dataoutputstream.close();
         }
     }
 
@@ -64,12 +61,9 @@ public class CompressedStreamTools {
     }
 
     public static void write(NBTTagCompound p_74795_0_, File p_74795_1_) throws IOException {
-        DataOutputStream dataoutputstream = new DataOutputStream(new FileOutputStream(p_74795_1_));
 
-        try {
+        try (DataOutputStream dataoutputstream = new DataOutputStream(new FileOutputStream(p_74795_1_))) {
             write(p_74795_0_, dataoutputstream);
-        } finally {
-            dataoutputstream.close();
         }
     }
 
