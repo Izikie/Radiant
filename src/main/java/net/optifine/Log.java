50d7113f70c78be5f80999c211caecde222cee20
Izikie <78509731+Izikie@users.noreply.github.com>
2025-05-25 19:41:28 -0500

    Cleanup (GameSettings, Log)

--- src/main/java/net/optifine/Log.java
+++ src/main/java/net/optifine/Log.java
@@ -6,33 +6,31 @@ import org.apache.logging.log4j.Logger;
 public class Log {
     private static final Logger LOGGER = LogManager.getLogger("Optifine");
 
-    public static void info(String s) {
-        LOGGER.info("{}", s);
+    public static void info(String message) {
+        LOGGER.info("{}", message);
     }
 
-    public static void info(String format, Object... args) {
-        String s = String.format(format, args);
-        LOGGER.info(s);
+    public static void info(String message, Object... args) {
+        LOGGER.info(message, args);
     }
 
-    public static void warn(String s) {
-        LOGGER.warn(s);
+    public static void warn(String message) {
+        LOGGER.warn(message);
     }
 
-    public static void warn(String format, Object... args) {
-        String s = String.format(format, args);
-        LOGGER.warn(s);
+    public static void warn(String message, Object... args) {
+        LOGGER.warn(message, args);
     }
 
-    public static void warn(String s, Throwable t) {
-        LOGGER.warn(s, t);
+    public static void warn(String message, Throwable throwable) {
+        LOGGER.warn(message, throwable);
     }
 
-    public static void error(String s) {
-        LOGGER.error(s);
+    public static void error(String message) {
+        LOGGER.error(message);
     }
 
-    public static void error(String s, Throwable t) {
-        LOGGER.error(s, t);
+    public static void error(String message, Throwable throwable) {
+        LOGGER.error(message, throwable);
     }
 }
