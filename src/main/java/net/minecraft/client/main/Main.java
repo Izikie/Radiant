bf5550f7ac2b02eb82b837638f733b88da23971d
Cal <drred96@outlook.com>
2025-08-01 13:22:51 +1000

    Reverted back to joptsimple for native image compatibility

--- src/main/java/net/minecraft/client/main/Main.java
+++ src/main/java/net/minecraft/client/main/Main.java
@@ -1,18 +1,14 @@
 package net.minecraft.client.main;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.converters.FileConverter;
-import com.beust.jcommander.converters.IntegerConverter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mojang.authlib.properties.PropertyMap;
 import com.mojang.authlib.properties.PropertyMap.Serializer;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import joptsimple.ValueConverter;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.main.GameConfiguration.DisplayInformation;
-import net.minecraft.client.main.GameConfiguration.FolderInformation;
-import net.minecraft.client.main.GameConfiguration.ServerInformation;
-import net.minecraft.client.main.GameConfiguration.UserInformation;
 import net.minecraft.util.Session;
 
 import java.io.File;
@@ -21,178 +17,145 @@ import java.net.InetSocketAddress;
 import java.net.PasswordAuthentication;
 import java.net.Proxy;
 import java.net.Proxy.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    private static class MinecraftArgs {
-        // User Information
-        @Parameter(names = {"--username", "-username"}, description = "Username")
-        private String username = "Player";
 
-        @Parameter(names = {"--uuid", "-uuid"}, description = "UUID")
-        private String uuid;
 
-        @Parameter(names = {"--accessToken", "-accessToken"}, description = "Access token", required = true)
-        private String accessToken;
-
-        @Parameter(names = {"--userType", "-userType"}, description = "User type")
-        private String userType = "legacy";
-
-        @Parameter(names = {"--userProperties", "-userProperties"}, description = "User properties")
-        private String userProperties = "{}";
-
-        @Parameter(names = {"--profileProperties", "-profileProperties"}, description = "Profile properties")
-        private String profileProperties = "{}";
-
-        // Display Information
-        @Parameter(names = {"--width", "-width"}, description = "Window width", converter = IntegerConverter.class)
-        private Integer width = 854;
-
-        @Parameter(names = {"--height", "-height"}, description = "Window height", converter = IntegerConverter.class)
-        private Integer height = 480;
-
-        @Parameter(names = {"--fullscreen", "-fullscreen"}, description = "Start in fullscreen mode")
-        private boolean fullscreen = false;
-
-        @Parameter(names = {"--checkGlErrors", "-checkGlErrors"}, description = "Check for OpenGL errors")
-        private boolean checkGlErrors = false;
-
-        // Folder Information
-        @Parameter(names = {"--gameDir", "-gameDir"}, description = "Game directory", converter = FileConverter.class)
-        private File gameDir = new File(".");
-
-        @Parameter(names = {"--resourcePackDir", "-resourcePackDir"}, description = "Resource pack directory", converter = FileConverter.class)
-        private File resourcePackDir;
-
-        @Parameter(names = {"--assetsDir", "-assetsDir"}, description = "Assets directory", converter = FileConverter.class)
-        private File assetsDir;
-
-        // Server Information
-        @Parameter(names = {"--server", "-server"}, description = "Server address")
-        private String server;
-
-        @Parameter(names = {"--port", "-port"}, description = "Server port", converter = IntegerConverter.class)
-        private Integer port = 25565;
-
-        // Proxy Information
-        @Parameter(names = {"--proxyHost", "-proxyHost"}, description = "Proxy host")
-        private String proxyHost;
-
-        @Parameter(names = {"--proxyPort", "-proxyPort"}, description = "Proxy port", converter = IntegerConverter.class)
-        private Integer proxyPort = 8080;
-
-        @Parameter(names = {"--proxyUser", "-proxyUser"}, description = "Proxy username")
-        private String proxyUser;
-
-        @Parameter(names = {"--proxyPass", "-proxyPass"}, description = "Proxy password")
-        private String proxyPass;
-
-        // Other
-        @Parameter(description = "Other arguments")
-        private List<String> otherArgs = new ArrayList<>();
-    }
 
     public static void main(String[] args) {
         System.setProperty("java.net.preferIPv4Stack", "true");
         System.setProperty("log4j2.formatMsgNoLookups", "true");
         System.setProperty("io.netty.allocator.maxOrder", "9"); // Default is 16MiB, Minecraft uses 2MiB, use 4MiB as safe default
 
-        MinecraftArgs minecraftArgs = new MinecraftArgs();
-        JCommander jCommander = JCommander.newBuilder()
-                .addObject(minecraftArgs)
-                .allowParameterOverwriting(true)
-                .programName("Minecraft")
-                .build();
+        OptionParser parser = new OptionParser();
+        parser.allowsUnrecognizedOptions();
+        parser.accepts("fullscreen");
+        parser.accepts("checkGlErrors");
+        OptionSpec<String> optionServer = parser.accepts("server").withRequiredArg();
+        OptionSpec<Integer> optionPort = parser.accepts("port").withRequiredArg().withValuesConvertedBy(new IntegerConverter()).defaultsTo(25565);
+        OptionSpec<File> optionGameDir = parser.accepts("gameDir").withRequiredArg().withValuesConvertedBy(new FileConverter()).defaultsTo(new File("."));
+        OptionSpec<File> optionAssetsDir = parser.accepts("assetsDir").withRequiredArg().withValuesConvertedBy(new FileConverter());
+        OptionSpec<File> optionResourcePackDir = parser.accepts("resourcePackDir").withRequiredArg().withValuesConvertedBy(new FileConverter());
+        OptionSpec<String> optionProxyHost = parser.accepts("proxyHost").withRequiredArg();
+        OptionSpec<Integer> optionProxyPort = parser.accepts("proxyPort").withRequiredArg().withValuesConvertedBy(new IntegerConverter()).defaultsTo(8080);
+        OptionSpec<String> optionProxyUser = parser.accepts("proxyUser").withRequiredArg();
+        OptionSpec<String> optionProxyPass = parser.accepts("proxyPass").withRequiredArg();
+        OptionSpec<String> optionUsername = parser.accepts("username").withRequiredArg().defaultsTo("Player");
+        OptionSpec<String> optionUuid = parser.accepts("uuid").withRequiredArg();
+        OptionSpec<String> optionAccessToken = parser.accepts("accessToken").withRequiredArg().required();
+        OptionSpec<Integer> optionWidth = parser.accepts("width").withRequiredArg().withValuesConvertedBy(new IntegerConverter()).defaultsTo(854);
+        OptionSpec<Integer> optionHeight = parser.accepts("height").withRequiredArg().withValuesConvertedBy(new IntegerConverter()).defaultsTo(480);
+        OptionSpec<String> optionUserProperties = parser.accepts("userProperties").withRequiredArg().defaultsTo("{}");
+        OptionSpec<String> optionProfileProperties = parser.accepts("profileProperties").withRequiredArg().defaultsTo("{}");
+        OptionSpec<String> optionUserType = parser.accepts("userType").withRequiredArg().defaultsTo("legacy");
+        OptionSpec<String> nonOptions = parser.nonOptions();
+        OptionSet options = parser.parse(args);
 
-        try {
-            jCommander.parse(args);
-        } catch (Exception exception) {
-            System.err.println("Error parsing command line arguments: " + exception.getMessage());
-            jCommander.usage();
-            return;
+        List<String> ignoredArgs = options.valuesOf(nonOptions);
+        if (!ignoredArgs.isEmpty()) {
+            System.out.println("Completely ignored arguments: " + ignoredArgs);
         }
 
-        if (!minecraftArgs.otherArgs.isEmpty()) {
-            System.out.println("Completely ignored arguments: " + minecraftArgs.otherArgs);
-        }
-
+        String proxyHost = options.valueOf(optionProxyHost);
         Proxy proxy = Proxy.NO_PROXY;
-        if (isNotNullOrEmpty(minecraftArgs.proxyHost)) {
-            try {
-                proxy = new Proxy(Type.SOCKS, new InetSocketAddress(minecraftArgs.proxyHost, minecraftArgs.proxyPort));
 
-                if (isNotNullOrEmpty(minecraftArgs.proxyUser) && isNotNullOrEmpty(minecraftArgs.proxyPass)) {
-                    Authenticator.setDefault(new Authenticator() {
-                        @Override
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(minecraftArgs.proxyUser, minecraftArgs.proxyPass.toCharArray());
-                        }
-                    });
-                }
-            } catch (Exception exception) {
-                System.err.println("Error configuring proxy: " + exception.getMessage());
+        if (proxyHost != null) {
+            try {
+                proxy = new Proxy(Type.SOCKS, new InetSocketAddress(proxyHost, options.valueOf(optionProxyPort)));
+            } catch (Exception ignored) {
+
             }
+        }
+
+        final String proxyUser = options.valueOf(optionProxyUser);
+        final String proxyPass = options.valueOf(optionProxyPass);
+
+        if (!proxy.equals(Proxy.NO_PROXY) && isNotNullOrEmpty(proxyUser) && isNotNullOrEmpty(proxyPass)) {
+            Authenticator.setDefault(new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(proxyUser, proxyPass.toCharArray());
+                }
+            });
         }
 
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(PropertyMap.class, new Serializer())
                 .create();
-        PropertyMap userProperties = gson.fromJson(minecraftArgs.userProperties, PropertyMap.class);
-        PropertyMap profileProperties = gson.fromJson(minecraftArgs.profileProperties, PropertyMap.class);
 
-        File gameDir = minecraftArgs.gameDir;
-        File assetsDir = minecraftArgs.assetsDir != null ?
-                minecraftArgs.assetsDir : new File(gameDir, "assets/");
-        File resourcePackDir = minecraftArgs.resourcePackDir != null ?
-                minecraftArgs.resourcePackDir : new File(gameDir, "resourcepacks/");
+        // User Info
+        String playerID = options.has(optionUuid) ? optionUuid.value(options) : optionUsername.value(options);
+        Session session = new Session(optionUsername.value(options), playerID, optionAccessToken.value(options), optionUserType.value(options));
+        PropertyMap userProperties = gson.fromJson(options.valueOf(optionUserProperties), PropertyMap.class);
+        PropertyMap profileProperties = gson.fromJson(options.valueOf(optionProfileProperties), PropertyMap.class);
 
-        String uuid = minecraftArgs.uuid != null ?
-                minecraftArgs.uuid : minecraftArgs.username;
+        // Display Info
+        int width = options.valueOf(optionWidth);
+        int height = options.valueOf(optionHeight);
+        boolean fullscreen = options.has("fullscreen");
+        boolean checkGlErrors = options.has("checkGlErrors");
 
-        Session session = new Session(
-                minecraftArgs.username,
-                uuid,
-                minecraftArgs.accessToken,
-                minecraftArgs.userType
-        );
+        // Folder Info
+        File gameDir = options.valueOf(optionGameDir);
+        File assetsDir = options.has(optionAssetsDir) ? options.valueOf(optionAssetsDir) : new File(gameDir, "assets/");
+        File resourcePackDir = options.has(optionResourcePackDir) ? options.valueOf(optionResourcePackDir) : new File(gameDir, "resourcepacks/");
 
-        GameConfiguration gameConfiguration = new GameConfiguration(
-                new UserInformation(
-                        session,
-                        userProperties,
-                        profileProperties,
-                        proxy
-                ),
-                new DisplayInformation(
-                        minecraftArgs.width,
-                        minecraftArgs.height,
-                        minecraftArgs.fullscreen,
-                        minecraftArgs.checkGlErrors
-                ),
-                new FolderInformation(
-                        gameDir,
-                        resourcePackDir,
-                        assetsDir
-                ),
-                new ServerInformation(
-                        minecraftArgs.server,
-                        minecraftArgs.port
-                )
+        // Server Info
+        String server = options.valueOf(optionServer);
+        Integer port = options.valueOf(optionPort);
+
+        GameConfiguration gameconfiguration = new GameConfiguration(
+                new GameConfiguration.UserInformation(session, userProperties, profileProperties, proxy),
+                new GameConfiguration.DisplayInformation(width, height, fullscreen, checkGlErrors),
+                new GameConfiguration.FolderInformation(gameDir, resourcePackDir, assetsDir),
+                new GameConfiguration.ServerInformation(server, port)
         );
 
         Runtime.getRuntime().addShutdownHook(new Thread("Client Shutdown Thread") {
-            @Override
             public void run() {
                 Minecraft.stopIntegratedServer();
             }
         });
-
-        Thread.currentThread().setName("Client Thread");
-        new Minecraft(gameConfiguration).run();
+        Thread.currentThread().setName("Client thread");
+        new Minecraft(gameconfiguration).run();
     }
 
     private static boolean isNotNullOrEmpty(String str) {
         return str != null && !str.isEmpty();
+    }
+
+    private static class IntegerConverter implements ValueConverter<Integer> {
+        @Override
+        public Integer convert(String value) {
+            return Integer.valueOf(value);
+        }
+
+        @Override
+        public Class<? extends Integer> valueType() {
+            return Integer.class;
+        }
+
+        @Override
+        public String valuePattern() {
+            return null;
+        }
+    }
+
+    private static class FileConverter implements ValueConverter<File> {
+
+        @Override
+        public File convert(String value) {
+            return new File(value);
+        }
+
+        @Override
+        public Class<? extends File> valueType() {
+            return File.class;
+        }
+
+        @Override
+        public String valuePattern() {
+            return null;
+        }
     }
 }
